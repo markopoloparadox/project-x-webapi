8ff94989a27c7edd176e021c5bf8baf366db0b9b
Marko Petrlic <petrlic.marko@gmail.com>
2022-11-11 22:30:39 +0100

    Added rustfmt and rust-toolchain files

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,35 @@
 use std::net::SocketAddr;
 
-use axum::routing::get;
-use axum::Router;
+use axum::{routing::get, Router};
 
 mod database;
 mod server;
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-    let db = database::Database::new();
-    let result = db.query("GET ALL FROM test WHERE family = Ana");
-    println!("{:?}", result);
+	// initialize tracing
+	tracing_subscriber::fmt::init();
+	let db = database::Database::new();
+	let result = db.query("GET ALL FROM test WHERE family = Ana");
+	println!("{:?}", result);
 
-    /*
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
+	/*
+	// build our application with a route
+	let app = Router::new()
+		// `GET /` goes to `root`
+		.route("/", get(root));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::info!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap(); */
+	// run our app with hyper
+	// `axum::Server` is a re-export of `hyper::Server`
+	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+	tracing::info!("listening on {}", addr);
+	axum::Server::bind(&addr)
+		.serve(app.into_make_service())
+		.await
+		.unwrap(); */
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
-    "Hello, World!"
+	"Hello, World!"
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -5,12 +5,12 @@ use crate::schema::{CPUListStorage, PriceListStorage};
 pub mod routes;
 
 pub async fn start(
-    addr: impl Into<SocketAddr>,
-    cpu_list: CPUListStorage,
-    price_list: PriceListStorage,
+	addr: impl Into<SocketAddr>,
+	cpu_list: CPUListStorage,
+	price_list: PriceListStorage,
 ) {
-    warp::serve(routes::make_routes(cpu_list, price_list))
-        .run(addr)
-        .await;
+	warp::serve(routes::make_routes(cpu_list, price_list))
+		.run(addr)
+		.await;
 }
  */
